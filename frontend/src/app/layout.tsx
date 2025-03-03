26c4b30706e48952e270e133e5cb7e99ce57ee93
Sourav Walke <sourav.wal@gmail.com>
2025-03-03 00:04:00 -0800

    feat: Add Vercel Speed Insights

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Bricolage_Grotesque } from "next/font/google"
 import './globals.css'
 import { Toaster } from 'sonner';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const bricolage = Bricolage_Grotesque({
   subsets: ["latin"],
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body className={`font-sans antialiased`}>
         <Toaster richColors position="top-center" />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
